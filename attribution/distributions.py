cc5b284a600972a4ebe2d3cc0e37db850cb62806
Klas Leino <kleino@live.com>
2019-04-21 01:59:31 -0400

    documentation for distributions

--- attribution/distributions.py
+++ attribution/distributions.py
@@ -1,4 +1,5 @@
 '''
+Docstring for the distributions module.
 '''
 
 import keras.backend as K
@@ -7,56 +8,73 @@ import keras.backend as K
 class Doi(object):
     '''
     Interface for a distribution of interest.
+
+    The *distribution of interest* lets us specify the set of instances over 
+    which we want our explanations to be faithful.
     '''
 
     def __call__(self, z):
         '''
-        Takes a tensor z, which is the input to z and returns a new tensor, z',
-        which has an entry for each point in the distribution of each instance.
+        Takes a tensor, z, which is the input to g (in model, f = g o h), and 
+        returns a new tensor, z', which has an entry for each point in the 
+        distribution of interest for each instance. The distribution of interest
+        is assumed be a uniform distribution over all points returned this way.
+
+        Parameters
+        ----------
+        z : keras.backend.Tensor
+            The tensor representing the output of the layer defining the slce.
+
+        Returns
+        -------
+        keras.backend.Tensor
+            A new tensor connected to `z`, which represents the distribution of
+            interest.
         '''
         raise NotImplementedError('This is an abstract method.')
 
     @staticmethod
     def linear_interp():
-        '''
-        '''
         return LinearInterpDoi()
 
     @staticmethod
     def point():
-        '''
-        '''
         return PointDoi()
 
 
 class LinearInterpDoi(Doi):
     '''
+    A distribution of interest, which, for point, z, is the uniform distribution
+    over the linear interpolation from a given baseline to z. This distribution
+    of interest yields the Aumann-Shapley value.
     '''
 
     def __call__(self, z):
+        # Make placeholders for the resolution and baseline.
         r = K.placeholder(name='resolution', ndim=0, shape=(), dtype='int32')
         baseline = K.placeholder(name='baseline', shape=K.int_shape(z)[1:])
 
         b = K.expand_dims(baseline, axis=0)
 
+        # Allocate the alpha term for the interpolation.
         a = K.tile(
             (1. + K.arange(r, dtype='float32')) / K.cast(r, 'float32'),
             [K.shape(z)[0]])
         for _ in range(K.ndim(z) - 1):
             a = K.expand_dims(a, axis=-1)
 
-        # K.repeat_elements has inconsistent behavior across backends
-        # For theano, it is fine to use a tensor for reps
-        # For tensorflow, it is not, and repeat_elements needs a Python integer
+        # K.repeat_elements has inconsistent behavior across backends. For 
+        # theano, it is fine to use a tensor for reps; for tensorflow, it is 
+        # not, and repeat_elements needs a Python integer.
         # The following hack for tensorflow is adapted from:
         #    https://github.com/keras-team/keras/issues/2656
         if K.backend() == 'theano':
             z_rep = K.repeat_elements(z, r, axis=0)
         elif K.backend() == 'tensorflow':
-            multiples = K.variable([r]+[1 for i in range(K.ndim(z)-1)], dtype='int32')
+            multiples = K.variable(
+                [r]+[1 for i in range(K.ndim(z)-1)], 
+                dtype='int32')
             z_rep = K.tf.tile(z, multiples)
-
-        #b = K.repeat_elements(b, z_rep.shape[0], axis=0)
 
         z_interp = b + a * (z_rep - b)
 
@@ -75,8 +93,11 @@ class LinearInterpDoi(Doi):
 
 class PointDoi(Doi):
     '''
+    A distribution of intest where all of the probability mass is on a single 
+    point.
     '''
 
     def __call__(self, z):
+        z._doi_parent = z
         z._doi_params = {}
         return z
