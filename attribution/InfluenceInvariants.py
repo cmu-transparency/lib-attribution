4f7df1c1d938c9ee9c6e60e7f6883444b0af6357
Matt Fredrikson <mfredrik@cmu.edu>
2019-06-23 12:14:21 -0400

    Fixed buggy defaults in InfluenceInvariants

--- attribution/InfluenceInvariants.py
+++ attribution/InfluenceInvariants.py
@@ -8,9 +8,10 @@ from sklearn import tree
 from .methods import InternalInfluence
 from .invariant import Literal, Clause, Invariant
 
+
 class InfluenceInvariants(object):
 
-    def __init__(self, model, layer=None, agg_fn=K.max, Q=None, multiply_activation=True):
+    def __init__(self, model, layer=None, agg_fn=None, Q=None, multiply_activation=True):
         self.model = model
         self.agg_fn = agg_fn
         self.layer = layer
@@ -21,7 +22,8 @@ class InfluenceInvariants(object):
         if Q is None:
             self.Q = K.argmax(model.output, axis=1)
 
-        self._attributer = InternalInfluence(model, self.layer, agg_fn=agg_fn, multiply_activation=multiply_activation)
+        self._attributer = InternalInfluence(
+            model, self.layer, agg_fn=agg_fn, multiply_activation=multiply_activation)
         self._is_compiled = False
 
     def _get_influence(self, x, batch_size=None):
@@ -30,7 +32,8 @@ class InfluenceInvariants(object):
         if np.ndim(x) == K.ndim(self.model.input) - 1:
             x = np.expand_dims(x, axis=0)
 
-        acts = np.sign(self._attributer.get_attributions(x, batch_size=batch_size)).reshape(len(x), -1)
+        acts = np.sign(self._attributer.get_attributions(
+            x, batch_size=batch_size)).reshape(len(x), -1)
 
         qs = self.QF(x)
 
@@ -42,18 +45,19 @@ class InfluenceInvariants(object):
         f_Q = K.function([self.model.input], [self.Q])
         self.QF = lambda x: f_Q([x])[0]
 
-        zero_input = np.zeros(shape=(1,)+K.int_shape(self.model.input)[1:])
-        self._attrib_shapes = self._attributer.get_attributions(zero_input).shape[1:]
+        zero_input = np.zeros(shape=(1,) + K.int_shape(self.model.input)[1:])
+        self._attrib_shapes = self._attributer.get_attributions(
+            zero_input).shape[1:]
         self._attrib_cards = int(np.prod(self._attrib_shapes))
 
-        feat_ranges = [(0, self._attrib_cards-1)]
+        feat_ranges = [(0, self._attrib_cards - 1)]
         self._feat_ranges = feat_ranges
 
         self._is_compiled = True
 
         return self
 
-    def get_invariants(self, x, min_support=None, min_precision=1.0, batch_size=None, **kwargs):
+    def get_invariants(self, x, min_support=None, min_precision=1.0, batch_size=1, **kwargs):
 
         assert self._is_compiled
 
@@ -69,18 +73,23 @@ class InfluenceInvariants(object):
             node, clause = stack.pop()
             if(clf.tree_.children_left[node] != clf.tree_.children_right[node]):
                 unit = clf.tree_.feature[node]
-                au = K.sign(self._attributer.symbolic_attributions[:,unit])
-                lit_f = Literal(self.layer, unit, K.less_equal, clf.tree_.threshold[node], attribution_unit=au)
-                lit_t = Literal(self.layer, unit, K.greater, clf.tree_.threshold[node], attribution_unit=au)
-                stack.append((clf.tree_.children_left[node], 
-                                clause.add_literal(lit_f, copy_self=True)))
-                stack.append((clf.tree_.children_right[node], 
-                                clause.add_literal(lit_t, copy_self=True)))
+                au = K.sign(self._attributer.symbolic_attributions[:, unit])
+                lit_f = Literal(self.layer, unit, K.less_equal,
+                                clf.tree_.threshold[node], attribution_unit=au)
+                lit_t = Literal(self.layer, unit, K.greater,
+                                clf.tree_.threshold[node], attribution_unit=au)
+                stack.append((clf.tree_.children_left[node],
+                              clause.add_literal(lit_f, copy_self=True)))
+                stack.append((clf.tree_.children_right[node],
+                              clause.add_literal(lit_t, copy_self=True)))
             else:
                 q = clf.tree_.value[node].argmax()
-                support = clf.tree_.value[node,0,q]/(len(np.where(y == q)[0]))
-                precision = clf.tree_.value[node,0,q]/clf.tree_.value[node].sum()
+                support = clf.tree_.value[node, 0,
+                                          q] / (len(np.where(y == q)[0]))
+                precision = clf.tree_.value[node, 0,
+                                            q] / clf.tree_.value[node].sum()
                 if (min_support is None or min_support <= support) and (min_precision is None or min_precision <= precision):
-                    invs.append(Invariant([clause], self._attributer.model, Q=q, support=support, precision=precision))
+                    invs.append(Invariant(
+                        [clause], self._attributer.model, Q=q, support=support, precision=precision))
 
         return invs
